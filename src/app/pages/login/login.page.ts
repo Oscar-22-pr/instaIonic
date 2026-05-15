5eb48b9430ad6c1868e523d9d7323090a2ae48e5
Oscar Julian Perez <oscar_perezqu@fet.edu.co>
2026-05-15 09:52:17 -0500

    Corrige login y validación del formulario

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  IonHeader, IonToolbar, IonTitle, IonContent,
-  IonItem, IonInput, IonButton, IonText,
-  IonSegment, IonSegmentButton, IonLabel, IonIcon } from '@ionic/angular/standalone';
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonText,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonIcon,
+} from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../../services/auth';
 import { Router } from '@angular/router';
@@ -13,44 +23,89 @@ import { Router } from '@angular/router';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonHeader, IonToolbar, IonTitle, IonContent,
-    IonItem, IonInput, IonButton, IonText,
-    IonSegment, IonSegmentButton, IonLabel,
-    FormsModule, CommonModule,]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonInput,
+    IonButton,
+    IonText,
+    IonSegment,
+    IonSegmentButton,
+    IonLabel,
+    IonIcon,
+  ],
 })
 export class LoginPage implements OnInit {
+  email = '';
+  password = '';
+  name = '';
+  username = '';
+  isRegister = false;
+  error = '';
 
-    email = ''; password = ''; name = ''; username = ''; isRegister = false; error = '';
+  constructor(private auth: Auth, private router: Router) {}
 
-    constructor(private auth: Auth, private router: Router) {}
+  ngOnInit(): void {}
 
-    ngOnInit(): void {}
+  isFormValid(): boolean {
+    if (this.isRegister) {
+      return !!this.name && !!this.username && !!this.email && !!this.password;
+    }
+    return !!this.email && !!this.password;
+  }
 
-    isFormValid(): boolean {
-      if (this.isRegister) {
-        return !!this.name && !!this.username && !!this.email && !!this.password;
-      }
+  private blurActiveElement() {
+    const active = document.activeElement as HTMLElement | null;
+    active?.blur();
+  }
 
-      return !!this.email && !!this.password;
+  submit() {
+    this.blurActiveElement();
+    this.error = '';
+
+    if (!this.isFormValid()) {
+      this.error = 'Completa todos los campos';
+      return;
     }
 
-    submit() {
-      if (this.isRegister) {
-        this.auth.register({
+    if (this.isRegister) {
+      this.auth
+        .register({
           name: this.name,
+          username: this.username,
           email: this.email,
           password: this.password,
-          username: this.username
-        }).subscribe({
-          next: res => { this.auth.setToken(res.token); this.router.navigateByUrl('/feed'); },
-          error: _ => this.error = 'No se pudo registrar'
+        })
+        .subscribe({
+          next: (res) => {
+            this.auth.setToken(res.token);
+            this.router.navigateByUrl('/feed');
+          },
+          error: () => {
+            this.error = 'No se pudo registrar';
+          },
         });
-      } else {
-        this.auth.login(this.email, this.password).subscribe({
-          next: res => { this.auth.setToken(res.token); this.router.navigateByUrl('/feed'); },
-          error: _ => this.error = 'Credenciales inválidas'
-        });
-      }
+    } else {
+      this.auth.login(this.email, this.password).subscribe({
+        next: (res) => {
+          this.auth.setToken(res.token);
+          this.router.navigateByUrl('/feed');
+        },
+        error: () => {
+          this.error = 'Credenciales inválidas';
+        },
+      });
     }
+  }
 
+  toggleMode() {
+    this.blurActiveElement();
+    this.isRegister = !this.isRegister;
+    this.error = '';
+  }
 }
